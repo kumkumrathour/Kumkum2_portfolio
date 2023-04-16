367508e4c2b3efec246db07c20a71c47f2f405af
kumkumrathour <neharathour9669535194@gmail.com>
2023-04-16 23:56:05 +0530

    finish

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
            {nav ? <FaTimes size={30}/> :<FaBars size={30}/>}
         </div>
 
-        {nav && <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full  h-screen bg-gradient-to-b from-black  to-gray-600 text-gray-500">
+        {nav && <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full   bg-gradient-to-b from-black  to-gray-600 text-gray-500">
         {links.map(({id,link})=>(
             <li key={id} className="px-4 cursor-pointer capitalize py-6 "><Link onClick={()=> setNav(!nav)} to={link} smooth duration={500}>{link}</Link></li>
             ))}
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,33 +1,52 @@
 import React from 'react';
-import arrayDestruct from '../Image/Image_more/arrayDestruct.jpg'
-import installNode from '../Image/Image_more/installNode.jpg';
-import NavBar from '../Image/Image_more/navbar.jpg';
-import reactSmooth from '../Image/Image_more/reactSmooth.jpg';
-import reactWeather from '../Image/Image_more/reactWeather.jpg';
-import usestate from '../Image/Image_more/usestate.jpg';
+
+import foodOrderItem from '../Image/Image_more/foodOrderItem.png'
+import expenceItem from '../Image/Image_more/expenceItem.png'
+import todoList from '../Image/Image_more/todoList.png'
+import portfolioMy from '../Image/Image_more/portfolioMy.png'
+import duplicateCharacter from '../Image/Image_more/duplicateCharacter.png'
+import tailwindPortfolio from '../Image/Image_more/tailwindPortfolio.png';
+
+
+
+
+// https://kumkumrathour.netlify.app/
+
 
 const Portfolio = () => {
   const portfolio=[{
     id:1,
-    src:arrayDestruct
+    src:foodOrderItem,
+   link:' https://lucent-duckanoo-52db3f.netlify.app/',
+   code:'https://github.com/kumkumrathour/foodOrderItemProject',
   },{
     id:2,
-    src:installNode,
+    src:expenceItem,
+    link:'https://peaceful-cat-dbd42b.netlify.app/',
+    code:'https://github.com/kumkumrathour/expense-manager-final/tree/master/src',
   },{
     id:3,
-    src:NavBar,
+    src:todoList,
+    link:'https://tooddle.netlify.app/',
+    code:'https://github.com/kumkumrathour/toodle',
   },
   {
     id:5,
-    src:reactSmooth,
+    src:portfolioMy,
+    link:'https://kumkum-portfolio.netlify.app/',
+    code:'https://github.com/kumkumrathour/Kumkum2_portfolio',
   },
   {
     id:6,
-    src:usestate,
+    src:duplicateCharacter,
+    link:'',
+    code:'https://github.com/kumkumrathour/duplicateRemover',
   },
   {
     id:8,
-    src:reactWeather,
+    src:tailwindPortfolio,
+    link:'https://kumkumrathour.netlify.app/',
+    code:'https://github.com/kumkumrathour/My_portfolio',
   }
 ]
 
@@ -35,7 +54,7 @@ const Portfolio = () => {
 
   return (
     <div name='portfolio'
-    className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+    className='bg-gradient-to-b from-black to-gray-800 w-full text-white '>
        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pd-8'>
           <p className = " text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
@@ -43,12 +62,12 @@ const Portfolio = () => {
         </div>
 
         <div className='grid sm-grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-        {portfolio.map(({id,src})=>(
+        {portfolio.map(({id,src,link,code})=>(
           <div  key={id}className='shadow-md shadow-gray-600 rounded-lg'>
             <img src={src} alt="" className='rounded-md duration-200 hover:scale-105 '/>
-            <div className='flex items-center justify-center '>
-              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 ">Demo</button>
-              <button className= " w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 ">Code</button>
+            <div  className='flex items-center justify-center '>
+              <a  href={link} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 ">Demo</a>
+              <a href={code} className= " w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 ">Code</a>
             </div>
           </div>
      ))}
@@ -58,3 +77,5 @@ const Portfolio = () => {
   );
 }
 export default Portfolio;
+
+
